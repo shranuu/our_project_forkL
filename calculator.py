d2187128b55f6f33d21e783b791a5fbcd71060cd
P N Parameswaran <SESA784256@se.com>
2024-11-27 12:11:29 +0530

    edit in both sub.py and calculator.py by PNP

--- calculator.py
+++ calculator.py
@@ -1,1 +1,12 @@
+from subtract import *
+from mul import *
+from add import *
+from division import *
 
+num1 = int(input())
+num2 = int(input))
+
+print(add(num1,num2))
+print(subtract(num1,num2))
+print(multiplyy(num1,num2))
+print(division(num1,num2))
